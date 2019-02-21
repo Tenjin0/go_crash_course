7964512df36c433a29b805a86e15a9d9e6c48779
Patrice <ppetit@wynd.eu>
2019-02-21 17:56:10 +0100

    Core: Reverse string

--- 03_packages/strutil/reverse.go
+++ 03_packages/strutil/reverse.go
@@ -1,11 +1,15 @@
 package strutil
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Reverse(s string) string {
 
+	runes := []rune(s)
+	fmt.Println(runes, len(runes))
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		fmt.Printf("i: %b, j: %b\n", s[i], s[j])
+		fmt.Printf("i: %c, j: %c\n", s[i], s[j])
 	}
-	return s
+	return string(runes)
 }
