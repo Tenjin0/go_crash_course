9941fe10516ef3b1debea7e95a14e8c2b9c74e1c
Patrice <ppetit@wynd.eu>
2019-03-07 18:41:53 +0100

    Defer: [WIP]

---
+++ 13_defer/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func largest(nums []int) {
+
+	max := nums[0]
+
+	finished := func() {
+		fmt.Println("Finished: Largest number in", nums, "is", max)
+	}
+
+	defer finished()
+
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+
+}
+
+func deferStack() {
+
+	name := "Maxime"
+	fmt.Println("Original name:", name)
+	fmt.Print("Defer stack of print characters: ")
+	defer fmt.Println()
+	for _, c := range []rune(name) {
+		defer fmt.Printf("%c", c)
+	}
+
+}
+func main() {
+	largest([]int{78, 109, 2, 563, 300})
+	deferStack()
+}
