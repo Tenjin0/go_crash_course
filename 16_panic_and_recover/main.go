ff57734c156c4ee425cd803b544cdefc15f95073
Patrice PETIT <ppetit@wynd.eu>
2019-03-12 18:51:02 +0100

    Panic: [WIP]

---
+++ 16_panic_and_recover/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Firstname string
+	Lastname  string
+}
+
+func (p *Person) changeName(firstname *string, lastname *string) {
+	defer fmt.Println("deferred call in fullName")
+	if firstname == nil {
+		panic("runtime error: first name cannot be nil")
+	} else {
+		p.Firstname = *firstname
+	}
+	if lastname == nil {
+		panic("runtime error: last name cannot be nil")
+	} else {
+		p.Lastname = *lastname
+	}
+
+	fmt.Println("returned normally from fullName")
+}
+func main() {
+	defer fmt.Println("deferred call in main")
+	toto := Person{"toto", "Dupont"}
+	// newFirstname := nil
+	newLastname := "Dupond"
+	toto.changeName(nil, &newLastname)
+	fmt.Println(toto)
+}
