d4355f4775845b0218149ce9a7e3223c3db7d73f
Patrice PETIT <patricepetit@hotmail.com>
2019-03-20 21:15:45 +0100

    feature/writefile: append

--- 20_writing_files/main.go
+++ 20_writing_files/main.go
@@ -78,6 +78,33 @@ func writeLineInFile() {
 
 }
 
+func appendToFile() {
+	f, err := os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	newLine := "File handling is easy"
+	_, err = fmt.Fprintln(f, newLine)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("file  appended successfully")
+}
+
+func writeConcurrently() {
+
+}
+
 func main() {
-	writeLineInFile()
+	writeConcurrently()
 }
