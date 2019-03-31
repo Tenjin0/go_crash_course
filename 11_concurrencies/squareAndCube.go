db8822732c84a43b7bbed0db98b619add5669461
Patrice PETIT <patricepetit@hotmail.com>
2019-03-31 19:24:45 +0200

    feature/channel: time version with channel

--- 11_concurrencies/squareAndCube.go
+++ 11_concurrencies/squareAndCube.go
@@ -1,33 +1,84 @@
 package main
 
-func calcSquares(num int, squareop chan int) {
-	sum := 0
-	for i := 0; i < num; i++ {
-		sum += i * i
-	}
-	squareop <- sum
+import (
+	"fmt"
+	"time"
+)
 
+func digits(number int, dchnl chan int) {
+
+	for number != 0 {
+		digits := number % 10
+		dchnl <- digits
+		number /= 10
+	}
+	close(dchnl)
 }
 
-func calcCubes(num int, cubeop chan int) {
+func calcSquares2(number int, squareop chan int) {
 
 	sum := 0
-	for i := 0; i < num; i++ {
-		sum += i * i * i
+	for number != 0 {
+		digits := number % 10
+		sum += digits
+		number /= 10
+	}
+	squareop <- sum
+}
+
+func calcCubes2(number int, cubeop chan int) {
+
+	sum := 0
+	for number != 0 {
+		digits := number % 10
+		sum += digits
+		number /= 10
+	}
+	cubeop <- sum
+}
+
+func calcSquares(number int, squareop chan int) {
+
+	sum := 0
+	dch := make(chan int)
+	go digits(number, dch)
+
+	for digit := range dch {
+		sum += digit * digit
 	}
 
+	squareop <- sum
+}
+
+func calcCubes(number int, cubeop chan int) {
+
+	sum := 0
+	dch := make(chan int)
+
+	go digits(number, dch)
+
+	for digit := range dch {
+		sum += digit * digit
+	}
 	cubeop <- sum
 }
 
 func sumSquaresAndCubes(square, cube int) int {
 
-	var op chan int
-	op = make(chan int)
+	var start time.Time
+	number := 589
+	sqrch := make(chan int)
+	cubech := make(chan int)
+	start = time.Now()
 
-	go calcSquares(square, op)
-	go calcCubes(cube, op)
+	go calcSquares2(number, sqrch)
+	go calcCubes2(number, cubech)
+	squares, cubes := <-sqrch, <-cubech
+	fmt.Println("Final output", squares+cubes, time.Now().Sub(start))
 
-	squares, cubes := <-op, <-op
+	go calcSquares(number, sqrch)
+	go calcCubes(number, cubech)
+	squares, cubes = <-sqrch, <-cubech
+	fmt.Println("Final output", squares+cubes, time.Now().Sub(start))
 
-	return squares + cubes
 }
