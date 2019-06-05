be6ce9cda42693bf5d936a06fbaacfaa9f53cc9e
Patrice PETIT <ppetitpatrice@gmail.com>
2019-06-05 21:32:05 +0200

    codewars: lcm[WIP]

--- codewars/commondenominators/main.go
+++ codewars/commondenominators/main.go
@@ -1,1 +1,37 @@
-// https://www.codewars.com/kata/54d7660d2daf68c619000d95
+package commondenominators
+
+// kata: https://www.codewars.com/kata/54d7660d2daf68c619000d95
+// example: https://www.youtube.com/watch?v=WdsvijS5MRA
+// lcm: https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+
+// Pgcd is used to find the pgcd between two numbers
+func Pgcd(a, b int) int {
+
+	var sup, inf int
+
+	if a > b {
+		sup = a
+		inf = b
+	} else if b > a {
+		sup = b
+		inf = a
+	}
+
+	var rest int
+
+	for {
+		rest = sup % inf
+		if rest == 0 {
+			break
+		}
+		sup = inf
+		inf = rest
+	}
+
+	return inf
+}
+
+func ConvertFracts(a [][]int) string {
+	// your code
+	return ""
+}
--- codewars/main.go
+++ codewars/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/Tenjin0/go_crash_course/codewars/aretheythesame"
+	"github.com/Tenjin0/go_crash_course/codewars/commondenominators"
 )
 
 func main() {
@@ -14,7 +14,39 @@ func main() {
 	// fmt.Println(rotateformax.MaxRot2(56789))
 	// results := whicharein.InArray(a1[:], a2[:])
 	// fmt.Println(results)
-	var a1 = []int{}
-	var a2 = []int{25, 49}
-	fmt.Println(aretheythesame.Comp(a1, a2))
+	var lst = [][]int{{69, 130}, {87, 1310}, {30, 40}}
+	var common_pgcd int = lst[0][1]
+	for i := 1; i < len(lst); i++ {
+		common_pgcd = commondenominators.Pgcd(common_pgcd, lst[i][1])
+	}
+	var lst2 [][]int
+
+	for i := 0; i < len(lst); i++ {
+
+		converted_fraction := []int{lst[i][0], lst[i][1]}
+
+		for j := 0; j < len(lst); j++ {
+
+			coeffmultiplier := lst[j][1] / common_pgcd
+			if i != j {
+				converted_fraction[0] = converted_fraction[0] * coeffmultiplier
+				converted_fraction[1] = converted_fraction[1] * coeffmultiplier
+			}
+		}
+		lst2 = append(lst2, converted_fraction)
+	}
+	fmt.Println(lst2)
+
+	for i := 0; i < len(lst2); i++ {
+		common_pgcd = commondenominators.Pgcd(lst2[i][0], lst2[i][1])
+		fmt.Println(common_pgcd)
+	}
+
+	// if common_pgcd != 1 {
+	// 	for i := 1; i < len(lst2); i++ {
+	// 		lst2
+	// 	}
+	// }
+
+	fmt.Println(lst)
 }
